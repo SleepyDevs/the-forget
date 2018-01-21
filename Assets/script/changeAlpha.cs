e89759c0cd36d0f5e3514c5cfa9cc7d5fc01c602
Amornpong Trakarnkulphun <gearng2468@gmail.com>
2018-01-21 22:27:29 +0700

    alpha test
    push only changeAlpha and TurnLight. I think that I will put these codes
    into player interactive

---
+++ Assets/script/changeAlpha.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeAlpha : MonoBehaviour {
+
+	public KeyCode increaseAlpha;
+	public KeyCode decreaseAlpha;
+	public float alphaLevel = .1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown (increaseAlpha)){
+			alphaLevel += .9f;}
+
+		else if(Input.GetKeyDown (decreaseAlpha)){
+			alphaLevel -= .9f;}
+		
+		GetComponent<Renderer>().material.color = new Color(245/255f, 255/255f, 152/255f, alphaLevel);
+
+	}
+
+}
